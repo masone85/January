daf5b0f5b6089bf9a5bdc1461ef3f95a2e80bf95
masone85 <113108800+masone85@users.noreply.github.com>
2023-02-19 21:19:01 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,3 +3,18 @@ let apiUrl =
   "https://api.shecodes.io/weather/v1/current?query=Lisbon&key=af8a02404a8co87d6b9be4abdat1baf3&units=metric";
 
 console.log(apiUrl);
+
+function search(city) {
+  let apiKey = "af8a02404a8co87d6b9be4abdat1baf3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?city=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
